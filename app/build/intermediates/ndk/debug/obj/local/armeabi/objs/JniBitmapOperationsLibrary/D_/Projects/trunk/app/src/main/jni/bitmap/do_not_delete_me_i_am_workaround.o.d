5c05e34c19ea8cd7daf192260d2fe3c424de6cf0
selience <loveselience@gmail.com>
2015-12-04 16:03:15 +0800

    remove build folder

--- app/build/intermediates/ndk/debug/obj/local/armeabi/objs/JniBitmapOperationsLibrary/D_/Projects/trunk/app/src/main/jni/bitmap/do_not_delete_me_i_am_workaround.o.d
+++
@@ -1,2 +0,0 @@
-D:\Projects\trunk\app\build\intermediates\ndk\debug\obj/local/armeabi/objs/JniBitmapOperationsLibrary/D_\Projects\trunk\app\src\main\jni\bitmap\do_not_delete_me_i_am_workaround.o: \
- D:\Projects\trunk\app\src\main\jni\bitmap\do_not_delete_me_i_am_workaround.c
